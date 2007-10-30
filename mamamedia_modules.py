1ff0efdc0007a786272b9d69039b5807e82501cf
C. Neves <cn@sueste.net>
2007-10-30 18:34:44 +0000

    Removed runtime dependency on MaMaMediaMenu (Creative Center). New icon. Bumped version to 1.

--- mamamedia_modules.py
+++ mamamedia_modules.py
@@ -1,21 +1,31 @@
 import os
 import sys
 
-propfile = os.path.expanduser("~/.sugar/default/org.worldwideworkshop.olpc.MMMPath")
-if os.path.exists(propfile):
-    mmmpath = file(propfile, 'rb').read()
-else:
-    mmmpath=os.path.normpath(os.path.join(os.path.split(__file__)[0], '..', 'MaMaMediaMenu.activity'))
-
-print ("MMMPath", mmmpath)
-
-sys.path.append(mmmpath)
-
-from mmm_modules import *
+cwd = os.path.split(__file__)[0]
 
 import gtk
 theme = gtk.icon_theme_get_default()
-theme.append_search_path(os.path.join(mmmpath, 'icons'))
+
+if os.path.exists(os.path.join(cwd, 'mmm_modules')):
+    # We are self contained
+    theme.append_search_path(os.path.join(cwd, 'mamamedia_icons'))
+    pass
+else:
+    # Working with shared code on MaMaMediaMenu
+
+    propfile = os.path.expanduser("~/.sugar/default/org.worldwideworkshop.olpc.MMMPath")
+    if os.path.exists(propfile):
+        mmmpath = file(propfile, 'rb').read()
+    else:
+        mmmpath=os.path.normpath(os.path.join(cwd, '..', 'MaMaMediaMenu.activity'))
+
+    #print ("MMMPath", mmmpath)
+
+    sys.path.append(mmmpath)
+    theme.append_search_path(os.path.join(mmmpath, 'icons'))
+
+from mmm_modules import *
+
 
 if __name__ == '__main__':
     gather_other_translations()
